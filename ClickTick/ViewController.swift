9c191183376e5a3ca4e2fcfd1134c7e10e0344a1
YASH <yash@YASHs-MacBook-Pro.local>
2015-05-23 23:53:58 +0530

    1. The counter in the label increments by 1 every time the button is pressed.

--- ClickTick/ViewController.swift
+++ ClickTick/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    var count = 0
+    var label:UILabel! //Exclamation mark indicates implicit unwrapped optionals
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
+        var label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        self.label = label
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState:.Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func incrementCount()
+    {
+        self.count++
+        self.count = "\(self.count)" //gives outlet to labels
+    }
 }
-
